4774bbf6205a1c6ec635d11e835b2d26b2d6b0a9
Matt Kneiser <mattman@umich.edu>
2015-06-11 02:31:35 -0400

    Old updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,19 @@
 # Softlinks all dotfiles in current directory to
 #  the user's home directory, except git files.
 
+#
+# Emacs Packages
+#
+# Auto-Complete
 version="1.3.1"
 extension=".tar.bz2"
 auto_complete="auto-complete-${version}"
 auto_complete_pkg="${auto_complete}${extension}"
+auto_complete_url="http://cx4a.org/pub/auto-complete/${auto_complete_pkg}"
+# Python-mode
+python_mode_url="http://bazaar.launchpad.net/~python-mode-devs/python-mode/python-mode/download/head:/2%40fb98634b-d22b-0410-a57a-e996bee27b70:trunk%252Fpython-mode:python-mode.el/python-mode.el"
+emacs_packages=($auto_complete_url $python_mode_url)
+
 install_from_source=0
 no_backup=0
 green=""
@@ -63,36 +72,12 @@ case $ostype in
 	;;
 esac
 
-
-set -e
-
-pushd "$(dirname "${bash_source}")"
-mkdir "$HOME/.emacs.d" || :
-mkdir "$HOME/tmp" || :
-
-set -x
-if [[ $install_from_source -eq 1 ]]; then
-    echo "Installing dotfiles from source..."
-    curl -O "http://cx4a.org/pub/auto-complete/${auto_complete_pkg}"
-    tar xvf "${auto_complete_pkg}"
-    pushd "${auto_complete}"
-    current_datetime=$(date +"%F_at_%T")
-    make install DIR=$HOME/.emacs.d/ &> "makeoutput.$current_datetime"
-    popd
-    rm -rf "${auto_complete_pkg}"
-else
-    if [[ -d $auto_complete ]]; then
-	cp -r ${auto_complete}/* "${HOME}/.emacs.d"
-    fi
-fi
-set +x
-
 function safely_softlink_file() {
     # Softlinks $1 to $2
-    #  or to ${HOME}/$1 if $2 isn't provided.
+    #  or $1 to ${HOME}/$1 if only one argument is given
     # Backs up the target ($2 or ${HOME}/$1) if $no_backup is 0.
     local file=$1
-    local to_location="$2"
+    local to_location=$2
     if [[ -z $to_location ]]; then
 	to_location="${HOME}/${file}"
     else
@@ -114,15 +99,57 @@ function safely_softlink_file() {
     ln -s "${PWD}/${file}" "${to_location}" && echo "...done"
 }
 
-echo -ne "\n"
 
+set -e
+pushd "$(dirname "${bash_source}")"
+
+# Ensure two dirs exist
+#  Ignore failure of these mkdir's by OR'ing with a no-op
+mkdir "$HOME/.emacs.d" || :
+mkdir "$HOME/tmp" || :
+
+#
+# Install Emacs Packages
+#
+set -x
+if [[ $install_from_source -eq 1 ]]; then
+    echo "Installing emacs packages from source..."
+    for pkg in $emacs_packages; do
+	curl -LO $pkg
+	tar xvf $pkg
+	pushd $pkg
+	current_datetime=$(date +"%F_at_%T")
+	make install DIR=$HOME/.emacs.d/ &> "makeoutput.$current_datetime"
+	popd
+	rm -rf "${auto_complete_pkg}"
+    done
+else
+    for pkg in $emacs_packages; do
+    if [[ -d $pkg ]]; then
+	cp -r ${auto_complete}/* "${HOME}/.emacs.d"
+    fi
+    done
+fi
+set +x
+
+#
+# Install dotfiles except .git and .gitignore
+#
+echo -ne "\n"
 for dotfile in $(find . -maxdepth 1 -name '.?*'); do
     dotfile=${dotfile#./} #strip leading ./ from paths
     if [[ $dotfile != ".gitignore" && $dotfile != ".git" ]]; then
 	safely_softlink_file ${dotfile}
+	local hostname=$(hostname)
+	if [[ $dotfile == ".bashrc" &&  -n $hostname ]]; then
+	    sed -e "s/MACHINE_NAME .*/${hostname}/" $dotfile
+	fi
     fi
 done
 
+#
+# Install non-dotfiles
+#
 safely_softlink_file template.py ${HOME}/tmp
 
 
